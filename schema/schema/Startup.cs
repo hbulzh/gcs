3cda2f21ee4293758052a153c66431003c6b7695
李林1 <1@DESKTOP-JOS9SJB>
2020-01-09 13:19:45 +0800

    初始化 未加数据库

---
+++ schema/schema/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(schema.Startup))]
+namespace schema
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

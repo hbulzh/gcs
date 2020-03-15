897e0d66d4a606603e5d1488b0482276b5c13b3e
李林1 <1@DESKTOP-JOS9SJB>
2020-03-15 16:11:49 +0800

    屏幕

---
+++ schema/schema/Dao/ScreenDao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace schema.Dao
+{
+    public class ScreenDao
+    {
+        private Entities db = new Entities();
+
+        
+    }
+}
